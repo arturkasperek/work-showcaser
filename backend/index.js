e19dd891f23abd3c30e3c57f96c70e6bf9461c87
Artur <arturmiks@gmail.com>
2018-06-14 21:27:22 +0200

    work in progress on deploy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.all('/', (req, res) => res.send('Hello World!'))
+
+app.all('/resource', (req, res) => res.send('Resource res'));
 
 app.listen(8081, () => console.log('Example app listening on port 8081!'));
